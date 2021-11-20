787e58cc6e7841c1bdba844eb4c2a81e4b900e2d
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-11-19 23:41:33 -0300

    update

--- app/screens/inicio/Home.jsx
+++ app/screens/inicio/Home.jsx
@@ -23,46 +23,47 @@ export default Home = () => {
 
   const [selectedCategory, setSelectedCategory] = useState()
   const [tiendas, setTiendas] = useState(Tiendas)
-  const [largo,setLargo] = useState(Math.ceil(Tiendas.length / 2))
+  const [largo, setLargo] = useState(Math.ceil(Tiendas.length / 2))
   const selectCategory = (categoria) => {
     let filterRestaurante = Tiendas.filter(x => x.categorias === categoria.filtro)
     setTiendas(filterRestaurante)
-    setLargo(filterRestaurante.length  < 3 ? 50 : Math.ceil(filterRestaurante.length / 2))
+    setLargo(filterRestaurante.length < 3 ? 50 : Math.ceil(filterRestaurante.length / 2))
     setSelectedCategory(categoria)
   }
 
   function renderCategorias() {
     return (
       <View style={{ flex: 0.70 }}>
-        <View style={{ flexDirection:'row',marginTop:13 }}>
-        <Text
-          style={{
-            marginLeft:15,
-            fontSize: 30,
-            color: "#5D5D5D",
-            fontFamily: "Karantina-Bold",
-          }}>
-          Secciones
-        </Text>
-        {
-        selectedCategory && 
-                <Text
-                style={{
-                  alignSelf:'center',
-                  fontSize: 11,
-                  color: "#5D5D5D",
-                  position: 'absolute',
-                  right: 10,
-                }}
-                onPress={() => {
-                  setTiendas(Tiendas)
-                  setLargo(Math.ceil(Tiendas.length / 2))
-                  setSelectedCategory(false)
-                }}
-                >
-                  Limpiar Filtros
-              </Text>
-        }
+        <View style={{ flexDirection: 'row', marginTop: 13 }}>
+          <Text
+            style={{
+              marginLeft: 15,
+              fontSize: 30,
+              color: "#5D5D5D",
+              fontFamily: "Karantina-Bold",
+            }}
+          >
+            Secciones
+          </Text>
+          {
+            selectedCategory &&
+            <Text
+              style={{
+                alignSelf: 'center',
+                fontSize: 11,
+                color: "#5D5D5D",
+                position: 'absolute',
+                right: 10,
+              }}
+              onPress={() => {
+                setTiendas(Tiendas)
+                setLargo(Math.ceil(Tiendas.length / 2))
+                setSelectedCategory(false)
+              }}
+            >
+              Limpiar Filtros
+            </Text>
+          }
         </View>
         <UL
           data={Categorias}
@@ -120,7 +121,7 @@ export default Home = () => {
           style={{
             marginLeft: 15,
             marginTop: 10,
-            paddingVertical:2,
+            paddingVertical: 2,
             fontSize: 35,
             color: "rgb(229,097,00)",
             fontFamily: "Karantina-Bold",
@@ -130,27 +131,27 @@ export default Home = () => {
           Restaurantes
         </Text>
         <ScrollView
-        horizontal
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false} 
-        >
-        <UL
-          key={largo}
-          data={tiendas}
+          horizontal
           showsVerticalScrollIndicator={false}
-          keyExtractor={item => item.id.toString()}
-          numColumns={largo}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={{
-                padding: 6,
-                justifyContent:'center',
-              alignItems:'center'
-              }}
-              onPress={() => navigation.navigate("Restaurante",{
-                item
-              })}
-            >
+          showsHorizontalScrollIndicator={false}
+        >
+          <UL
+            key={largo}
+            data={tiendas}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={item => item.id.toString()}
+            numColumns={largo}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{
+                  padding: 6,
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+                onPress={() => navigation.navigate("Restaurante", {
+                  item
+                })}
+              >
                 <Image
                   source={item.foto}
                   resizeMode="cover"
@@ -167,8 +168,8 @@ export default Home = () => {
                     height: 20,
                     width: "80%",
                     backgroundColor: "white",
-                    borderTopRightRadius:50,
-                    borderTopLeftRadius:50,
+                    borderTopRightRadius: 50,
+                    borderTopLeftRadius: 50,
                     alignItems: 'center',
                     justifyContent: 'center',
                     flexDirection: "row"
@@ -187,8 +188,8 @@ export default Home = () => {
                   </Text>
                 </View>
               </TouchableOpacity>
-          )}
-        />
+            )}
+          />
         </ScrollView>
       </View>
     )
@@ -199,7 +200,7 @@ export default Home = () => {
       <Search />
       {renderCategorias()}
       {renderTiendas()}
-      <Footer/>
+      <Footer />
     </SafeAreaView>
   )
 }
